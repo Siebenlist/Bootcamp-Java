e6219903a9ec75d06f01b7ccdd5f3fd8fba9e2ae
Santiago Nicolas Rodriguez <48725347+Siebenlist@users.noreply.github.com>
2023-07-11 21:41:41 -0300

    Add files via upload

---
+++ Ejercicios_Java/Mi_primer_Programa/PrimerPrograma.java
@@ -0,0 +1,10 @@
+
+public class PrimerPrograma {
+    public static void main(String[] args){
+        //Imprime multiples Strings con mi nombre, edad y ciudad natal
+        System.out.println("Mi nombre es Santiago");
+        System.out.println("Tengo 21 años de edad");
+        System.out.println("Mi ciudad natal es Entre Rios, ARG"); 
+    }
+
+}
