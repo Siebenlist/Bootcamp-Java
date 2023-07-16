20d69d200c82c1b386b694999b39aab0a6aa7c52
Santiago Nicolas Rodriguez <48725347+Siebenlist@users.noreply.github.com>
2023-07-15 22:00:49 -0300

    Add files via upload

---
+++ Ejercicios_Java/zoologico/zooTest.java
@@ -0,0 +1,11 @@
+public class zooTest{
+    public static void main(String[] args){
+        Gorilla g = new Gorilla();
+        g.throwSomething();
+        g.throwSomething();
+        g.throwSomething();
+        g.eatBananas();
+        g.eatBananas();
+        g.climb();
+    }
+}
